e7a87e858e01cedcfd84c0c952c37f1b254e631d
Makhambet <krutchdd@gmail.com>
2021-02-08 14:25:26 +0600

    added registration form

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function get_alert(){
     alert("Yay!");
 }
 alert_btn.onclick = get_alert();
+var reg_btn = document.getElementById("reg");
+function get_reg(){
+    alert("you are registered!");
+}
+alert_btn.onclick = get_alert();
+reg_btn.onclick = get_reg();
